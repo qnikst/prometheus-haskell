ed74adfffd0a7c9388ffeae3a1467d12d458de8a
Alexander Vershilov <alexander.vershilov@sirius.online>
2020-06-02 02:09:35 +0300

    Drop ghc before 8.4

--- prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
+++ prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
@@ -43,132 +43,16 @@ ghcMetrics = ghcMetricsWithLabels []
 ghcMetricsWithLabels :: LabelPairs -> Metric GHCMetrics
 ghcMetricsWithLabels labels = Metric (do
   statsEnabled <-
-#if __GLASGOW_HASKELL__ < 804
-    getGCStatsEnabled
-#else
     getRTSStatsEnabled
-#endif
   if statsEnabled
   then return (GHCMetrics, do
         stats <-
-#if __GLASGOW_HASKELL__ < 804
-            getGCStats
-#else
             getRTSStats
-#endif
         extra <- getExtraStats
         concat <$> mapM (\f -> f labels stats) ghcCollectors
     )
   else return (GHCMetrics, return [])
   )
-
-#if __GLASGOW_HASKELL__ < 804
-ghcCollectors :: [LabelPairs -> GCStats -> IO [SampleGroup]]
-ghcCollectors = [
-        \labelpairs gcstats -> do
-          sparkCount <- numSparks
-          showCollector
-            "ghc_sparks"
-            "The number of sparks in the local spark pool."
-            GaugeType
-            sparkCount
-            labelpairs
-    ,   \labelpairs gcstats -> do
-          numCapabilities <- getNumCapabilities
-          showCollector
-            "ghc_capabilities"
-            "The number of threads that can run truly simultaneously."
-            GaugeType
-            numCapabilities
-            labelpairs
-    ,   statsCollector
-            "ghc_allocated_bytes_total"
-            "Total number of bytes allocated."
-            CounterType
-            bytesAllocated
-    ,   statsCollector
-            "ghc_num_gcs"
-            "The number of garbage collections performed."
-            CounterType
-            numGcs
-    ,   statsCollector
-            "ghc_max_used_bytes"
-            "The maximum number of live bytes seen so far."
-            GaugeType
-            maxBytesUsed
-    ,   statsCollector
-            "ghc_cumulative_used_bytes_total"
-            "The cumulative total bytes used."
-            CounterType
-            cumulativeBytesUsed
-    ,   statsCollector
-            "ghc_copied_bytes_total"
-            "The number of bytes copied during garbage collection."
-            CounterType
-            bytesCopied
-    ,   statsCollector
-            "ghc_current_used_bytes"
-            "The number of current live bytes."
-            GaugeType
-            currentBytesUsed
-    ,   statsCollector
-            "ghc_current_slop_bytes"
-            "The current number of bytes lost to slop."
-            GaugeType
-            currentBytesSlop
-    ,   statsCollector
-            "ghc_max_slop_bytes"
-            "The maximum number of bytes lost to slop so far."
-            GaugeType
-            maxBytesSlop
-    ,   statsCollector
-            "ghc_peak_allocated_megabytes" -- XXX: export as bytes?
-            "The maximum number of megabytes allocated."
-            GaugeType
-            peakMegabytesAllocated
-    ,   statsCollector
-            "ghc_mutator_cpu_seconds_total"
-            "The CPU time spent running mutator threads."
-            CounterType
-            mutatorCpuSeconds
-    ,   statsCollector
-            "ghc_mutator_wall_seconds_total"
-            "The wall clock time spent running mutator threads."
-            CounterType
-            mutatorCpuSeconds
-    ,   statsCollector
-            "ghc_gc_cpu_seconds_total"
-            "The CPU time spent running GC."
-            CounterType
-            gcCpuSeconds
-    ,   statsCollector
-            "ghc_gc_wall_seconds_total"
-            "The wall clock time spent running GC."
-            CounterType
-            gcWallSeconds
-    ,   statsCollector
-            "ghc_cpu_seconds_total"
-            "Total CPU time elapsed since program start."
-            CounterType
-            cpuSeconds
-    ,   statsCollector
-            "ghc_wall_seconds_total"
-            "Total wall clock time elapsed since start."
-            CounterType
-            wallSeconds
-    ,   statsCollector
-            "ghc_parallel_copied_bytes_total"
-            "Number of bytes copied during GC, minus space held by mutable lists held by the capabilities."
-            CounterType
-            parTotBytesCopied
-    ,   statsCollector
-            "ghc_parallel_max_copied_bytes_total"
-            "Sum of number of bytes copied each GC by the most active GC thread each GC."
-            CounterType
-            parMaxBytesCopied
-    ]
-
-#else
 
 ghcCollectors :: [LabelPairs -> RTSStats -> IO [SampleGroup]]
 ghcCollectors = [
@@ -336,19 +220,11 @@ ghcCollectors = [
 -- | Convert from 'RtsTime' (nanoseconds) to seconds with nanosecond precision.
 rtsTimeToSeconds :: Stats.RtsTime -> Fixed E9
 rtsTimeToSeconds = (/ 1e9) . fromIntegral
-#endif
 
-#if __GLASGOW_HASKELL__ < 804
-statsCollector :: Show a
-               => Text -> Text -> SampleType -> (GCStats -> a) -> LabelPairs -> GCStats -> IO [SampleGroup]
-statsCollector name help sampleType stat labels gcstats =
-    showCollector name help sampleType (stat gcstats) labels
-#else
 statsCollector :: Show a
                => Text -> Text -> SampleType -> (RTSStats -> a) -> LabelPairs -> RTSStats -> IO [SampleGroup]
 statsCollector name help sampleType stat labels rtsStats =
     showCollector name help sampleType (stat rtsStats) labels
-#endif
 
 showCollector :: Show a => Text -> Text -> SampleType -> a -> LabelPairs -> IO [SampleGroup]
 showCollector name help sampleType value labels = do
